9e246c052576191a66c2966956f28c02c3a38069
Mathew-Estafanous <mathewestafanous13@gmail.com>
2021-12-25 10:37:29 -0500

    open command will read general information about all files in dir.

--- cmd/open.go
+++ cmd/open.go
@@ -1,6 +1,10 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
 
 var openCmd = &cobra.Command{
 	Use: "open",
@@ -10,11 +14,56 @@ All folders and files within the current directory will be synced with the conne
 	
 When a bridge has successfully opened, you will be prompted with the Session ID that can
 be shared with those you wish to connect with.`,
-	Run: func(cmd *cobra.Command, args []string) {
-
-	},
+	Run: runOpen,
 }
 
 func init() {
 	rootCmd.AddCommand(openCmd)
+}
+
+func runOpen(cmd *cobra.Command, args []string) {
+	files, err := allFilesWithinDirectory(".")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(len(files))
+}
+
+type FileData struct {
+	os.FileInfo
+	parent string // the parent directory.
+}
+
+func (f FileData) String() string {
+	return f.parent + "/" + f.Name()
+}
+
+func allFilesWithinDirectory(dir string) ([]FileData, error) {
+	dirInfo, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get directory info: %v", err)
+	}
+
+	info := make([]FileData, 0, len(dirInfo))
+	for _, d := range dirInfo {
+		if d.IsDir() {
+			childInfo, err := allFilesWithinDirectory(dir + "/" + d.Name())
+			if err != nil {
+				fmt.Printf("Failed to get info of child directory %s", dir+"/"+d.Name())
+				continue
+			}
+			info = append(info, childInfo...)
+		} else {
+			fileInfo, err := d.Info()
+			if err != nil {
+				fmt.Printf("Failed to get file info of file %s", d.Name())
+				continue
+			}
+			info = append(info, FileData{
+				FileInfo: fileInfo,
+				parent: dir,
+			})
+		}
+	}
+	return info, nil
 }
